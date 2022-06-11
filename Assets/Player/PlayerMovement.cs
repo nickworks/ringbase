143aa6eea5479d5c6043fb505d680d97525d0341
Nick Pattison <pattison.nick@gmail.com>
2022-06-10 23:41:56 -0400

    Added jumping, rolling
    Reprogrammed character to use Rigidbody instead of CharacterController

--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -2,20 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour
 {
 
     public float walkSpeed = 5f;
     public float jetpackSpeed = 15f;
+    public float jumpImpulse = 10f;
     public Vector2 lookSensitivity = new Vector2(2, -2);
     private float yaw = 0;
     private float pitch = 0;
-    private CharacterController pawn;
+    private float roll = 0;
+    private Rigidbody body;
     private Camera cam;
 
     bool gravityIsOn = false;
-    Vector3 gravityDirection = Vector3.down;
+    bool playerIsGrounded = false;
+    Vector3 gravityDirection = Vector3.right;
     float gravityAmount = 10;
     Vector3 lastPosition = Vector3.zero;
 
@@ -23,29 +26,34 @@ public class PlayerMovement : MonoBehaviour
 
     void Start()
     {
-        pawn = GetComponent<CharacterController>();
+        body = GetComponent<Rigidbody>();
         cam = GetComponentInChildren<Camera>();
         lastPosition = transform.position;
     }
 
     void Update()
     {
-        DoLook();
 
         //CalcVelocity();
 
-        if(gravityIsOn)
+        if(gravityIsOn){
+            DoLookGravity();
             DoMoveGravity();
-        else
+        } else {
+            DoLookZeroG();
             DoMoveZeroG();
+        }
 
         if(Input.GetButtonDown("Gravity")) {
             gravityIsOn = !gravityIsOn;
             print($"gravity {(gravityIsOn?"on":"off")}");
         }
     }
+    void FixedUpdate(){
+        playerIsGrounded = false;
+    }
 
-    private void DoLook()
+    private void DoLookGravity()
     {
         float mx = Input.GetAxisRaw("Mouse X");
         float my = Input.GetAxisRaw("Mouse Y");
@@ -53,25 +61,44 @@ public class PlayerMovement : MonoBehaviour
         yaw += mx * lookSensitivity.x;
         pitch += my * lookSensitivity.y;
 
-        transform.eulerAngles = new Vector3(0, yaw, 0);
+        transform.rotation = Quaternion.FromToRotation(Vector3.down, gravityDirection) * Quaternion.Euler(0, yaw, 0);
         cam.transform.localEulerAngles = new Vector3(pitch, 0, 0);
     }
+    private void DoLookZeroG()
+    {
+        float mx = Input.GetAxisRaw("Mouse X");
+        float my = Input.GetAxisRaw("Mouse Y");
+        float lookRoll = Input.GetAxisRaw("LookRoll");
+
+
+        float ry = mx * lookSensitivity.x;
+        float rx = my * lookSensitivity.y;
+        float rz = lookRoll;
+
+        transform.localRotation *= Quaternion.Euler(rx,ry,rz);
+        cam.transform.localEulerAngles = new Vector3(0, 0, 0);
+    }
+
 
     private void DoMoveGravity()
     {
         float z = Input.GetAxisRaw("MoveForward");
         float x = Input.GetAxisRaw("MoveRight");
 
+        bool jump = Input.GetButtonDown("Jump");
+
         Vector3 input = transform.forward * z + transform.right * x;
         if (input.sqrMagnitude > 1) input.Normalize();
 
-        if(!pawn.isGrounded){
-            velocity += gravityDirection * gravityAmount * Time.deltaTime;
+        if(!playerIsGrounded){
+            body.velocity += gravityDirection * gravityAmount * Time.deltaTime;
         } else {
-            velocity = input * walkSpeed;
+            body.velocity += input * walkSpeed * Time.deltaTime;
+            if(jump){
+                body.velocity += 5 * -gravityDirection;
+                playerIsGrounded = false;
+            }
         }
-        
-        pawn.Move(velocity * Time.deltaTime);
     }
     private void DoMoveZeroG(){
 
@@ -83,8 +110,11 @@ public class PlayerMovement : MonoBehaviour
         Vector3 input = cam.transform.forward * z + cam.transform.right * x + cam.transform.up * y;
         //if (input.sqrMagnitude > 1) input.Normalize();
         
-        velocity += input * jetpackSpeed * Time.deltaTime;
-        pawn.Move(velocity * Time.deltaTime);
+        if(playerIsGrounded){
+            print("zero G but on the ground");
+        }
+
+        body.velocity += input * jetpackSpeed * Time.deltaTime;
     }
     void OnControllerColliderHit(ControllerColliderHit hit){
         CalcVelocity();
@@ -94,5 +124,17 @@ public class PlayerMovement : MonoBehaviour
         lastPosition = transform.position;
         if(v.sqrMagnitude < velocity.sqrMagnitude) velocity = v;
     }
+    void OnCollisionEnter (Collision collision) {
+		EvaluateCollision(collision);
+	}
 
+	void OnCollisionStay (Collision collision) {
+		EvaluateCollision(collision);
+	}
+    void EvaluateCollision (Collision collision) {
+		for (int i = 0; i < collision.contactCount; i++) {
+			Vector3 normal = collision.GetContact(i).normal;
+            playerIsGrounded |= Vector3.Dot(normal, gravityDirection) <= -.9f;
+		}
+	}
 }
